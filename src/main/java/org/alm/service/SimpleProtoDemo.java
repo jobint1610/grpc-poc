db1775192f1c1fc076b0e47ad6ca538474a27b3d
Jobin Thomas <jobin.thomas@almosafer.com>
2025-11-10 16:06:58 +0400

    Proto Mutability

--- src/main/java/org/alm/service/SimpleProtoDemo.java
+++ src/main/java/org/alm/service/SimpleProtoDemo.java
@@ -10,12 +10,27 @@ public class SimpleProtoDemo {
 
   public static void main(String[] args) {
 
-    var person = Person.newBuilder()
+    var person1 = Person.newBuilder()
             .setName("John Doe")
             .setAge(25)
             .build();
 
-    log.info("Person: {}", person);
+    log.info("person1: {}", person1);
+
+    // Create a new Person by modifying person1
+    var person2 = person1.toBuilder()
+            .setName("Jane Smith")
+            .build();
+
+    log.info("person2: {}", person2);
+
+    // Create a third Person by setting the age of person2 to null
+    // Note: Since protobuf does not support null, we need to clear the field
+    var person3 = person2.toBuilder()
+            .clearAge()
+            .build();
+
+    log.info("person3: {}", person3);
 
   }
 
