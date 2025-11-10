c1607a9d69920f65893a83761dc9064533f926a9
Jobin Thomas <jobin.thomas@almosafer.com>
2025-11-10 16:53:05 +0400

    Map Demo

---
+++ src/main/java/org/alm/service/MapProtoDemo.java
@@ -0,0 +1,35 @@
+package org.alm.service;
+
+import com.alm.model.Car;
+import com.alm.model.Dealer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MapProtoDemo {
+
+  private static final Logger log = LoggerFactory.getLogger(MapProtoDemo.class);
+
+  public static void main(String[] args) {
+
+    var car1 = Car.newBuilder()
+            .setMake("Honda")
+            .setModel("Civic")
+            .setYear(2015)
+            .build();
+
+    var car2 = Car.newBuilder()
+            .setMake("Toyota")
+            .setModel("Camry")
+            .setYear(2018)
+            .build();
+
+    var dealer = Dealer.newBuilder()
+            .setName("Best Cars Dealer")
+            .putInventory(car1.getYear(), car1)
+            .putInventory(car2.getYear(), car2)
+            .build();
+
+    log.info("dealer: {}", dealer);
+
+  }
+}
